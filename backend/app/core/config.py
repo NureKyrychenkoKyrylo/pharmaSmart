47b56ca14b2120e8a7e6843d45d78fc1441a596d
kerio1 <he4toki@gmail.com>
2025-12-11 16:36:27 +0200

    change

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,21 +1,15 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "PharmaSmart API"
     VERSION: str = "1.0.0"
     
-    DATABASE_URL: str = (
-        "postgresql://neondb_owner:"
-        "npg_kBYo5QdEp8xX"
-        "@ep-royal-flower-ageqyrt3-pooler.c-2.eu-central-1.aws.neon.tech/"
-        "neondb"
-    )
-
-    SECRET_KEY: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+    DATABASE_URL: str
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 120
 
-    class Config:
-        env_file = ".env"
+model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 settings = Settings()
